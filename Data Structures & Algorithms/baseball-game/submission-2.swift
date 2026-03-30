5ee447ea2be148e7dfdc39d9b98d991187c14def
Arjun Shukla <arjunshukla@users.noreply.github.com>
2026-03-29 20:04:52 -0700

    Add: baseball-game - submission-2

---
+++ Data Structures & Algorithms/baseball-game/submission-2.swift
@@ -0,0 +1,28 @@
+class Solution {
+    func calPoints(_ operations: [String]) -> Int {
+        var sum: [Int] = []
+
+        for op in operations {
+            switch op {
+                case "D":
+                    if let last = sum.last {
+                        sum.append(last * 2)
+                    }
+                case "+":
+                    if let last = sum.last,
+                   sum.count - 2 >= 0 {
+                    let secondLast = sum[sum.count - 2]
+                    sum.append(last + secondLast)
+                }
+                case "C":
+                    if !sum.isEmpty {
+                        sum.removeLast()
+                    }
+                default:
+                    sum.append(Int(op) ?? 0)
+            }
+        }
+
+        return sum.reduce(0, +)
+    }
+}
