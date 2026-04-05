a740a0efbf84082034567d364968f1f9c504d55d
Arjun Shukla <arjunshukla@users.noreply.github.com>
2026-04-04 21:51:54 -0700

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func longestConsecutive(_ nums: [Int]) -> Int {
+        var set = Set(nums)
+        var res = 0
+
+        for n in nums {
+            var len = 1
+
+            while set.contains(n + len) {
+                len += 1
+            }
+
+            res = max(res, len)
+        }
+
+        return res
+    }
+}
