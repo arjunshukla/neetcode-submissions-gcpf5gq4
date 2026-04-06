a4f3db67f5425b75352b9b4b8ddd04e633997975
Arjun Shukla <arjunshukla@users.noreply.github.com>
2026-04-06 15:28:22 -0700

    Add: path-sum - submission-6

---
+++ Data Structures & Algorithms/path-sum/submission-6.swift
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     var val: Int
+ *     var left: TreeNode?
+ *     var right: TreeNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    // DFS iterative
+    func hasPathSum(_ root: TreeNode?, _ targetSum: Int) -> Bool {
+        guard let root = root else { return false }
+
+        var stack: [(TreeNode, Int)] = [(root, targetSum - root.val)]
+
+        while !stack.isEmpty {
+            let (node, currSum) = stack.removeLast()
+
+            if node.left == nil 
+            && node.right == nil 
+            && currSum == 0 {
+                return true
+            }
+
+            if let left = node.left {
+                stack.append((left, currSum - left.val))
+            }
+
+            if let right = node.right {
+                stack.append((right, currSum - right.val))
+            }
+        }
+        return false
+    }
+}
