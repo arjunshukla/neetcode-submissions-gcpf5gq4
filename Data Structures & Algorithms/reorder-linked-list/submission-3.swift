6e097fc574357245efff53c783e39a907511f800
Arjun Shukla <arjunshukla@users.noreply.github.com>
2026-04-05 17:47:23 -0700

    Add: reorder-linked-list - submission-3

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-3.swift
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     var val: Int
+ *     var next: ListNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func reorderList(_ head: ListNode?) {
+        var curr = head
+        var arr: [ListNode] = []
+
+        while curr != nil {
+            arr.append(curr!)
+            curr = curr?.next
+        }
+
+        var L = 0
+        var R = arr.count - 1
+
+        while L < R {
+            arr[L].next = arr[R]
+            L += 1
+
+            arr[R].next = arr[L]
+            R -= 1
+        }
+        arr[L].next = nil
+    }
+}
