95ac132b82a393272482c121d51a9a2b23ee9073
Arjun Shukla <arjunshukla@users.noreply.github.com>
2026-04-08 21:17:35 -0700

    Add: contains-duplicate-ii - submission-3

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-3.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func containsNearbyDuplicate(_ nums: [Int], _ k: Int) -> Bool {
+        var set = Set<Int>()
+
+        for i in 0..<nums.count {
+            if set.contains(nums[i]) {
+                return true
+            }
+
+            set.insert(nums[i])
+
+            if set.count > k {
+                set.remove(nums[i - k])
+            }
+        }
+        return false 
+    }
+}
